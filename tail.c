ecd91a229c7c39025de9dafb15045246b78d0aee
tada66 <horaktada@seznam.cz>
2024-04-16 13:41:10 +0200

    working cbuf_create and cbuf_put

--- tail.c
+++ tail.c
@@ -37,29 +37,36 @@ cbuf* cbuf_create(int n){
 
 void cbuf_put (cbuf* cbuffer, char* line) 
 {
-    
+    int i = 0;
+    while(line[i]!='\0' && i<2047){
+        cbuffer->buffer[cbuffer->writeIndex].c[i]=line[i];
+        i++;
+    }
+    cbuffer->buffer[cbuffer->writeIndex].c[2047]='\0';
+    cbuffer->writeIndex++;
 }
+
+
 
 int main (int argc, char** argv)
 {
     int lines = 10;
     FILE *file;
     if(strcmp(argv[1], "-n") == 0){
-        if(!isdigit(argv[2])){
-            fprintf(stderr, "number of lines: '%s'", argv[2]);
+        if(!isdigit(*argv[2])){
+            fprintf(stderr, "number of lines: '%s'\n", argv[2]);
             exit(1);
         }
         lines = atoi(argv[2]);
         if(lines == 0){
             exit(0);
         }
+        if(argc=3)
+            file = stdin;
     }
-    printf("%d\n", lines);
-    cbuf* cbuffer = cbuf_create(2);
-    for(int i=0; i<2; i++){
-        cbuffer->buffer[i].c[0] = 'b';
-        printf("%s\n", cbuffer->buffer[i].c);
-    }
+    printf("cbuffer size=%d\n", lines);
+    cbuf* cbuffer = cbuf_create(lines);
+    
     int value = 1001;
     /*while (put (value ++));
     while (get (& value))
