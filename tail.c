1985ebb1c33dc2e01c52aa01bb3c8514dd983f95
tada66 <horaktada@seznam.cz>
2024-04-16 14:00:16 +0200

    working open file

--- tail.c
+++ tail.c
@@ -12,20 +12,6 @@ int readIndex;
 int bufferSize;
 } cbuf;
 
-/*
-int cbuf_get (int * value) 
-{
-  if (readIndex == writeIndex)
-  {
-     // buffer is empty
-     return 0;
-  }
-
-  *value = buffer[readIndex];
-  readIndex = (readIndex + 1) % N;
-  return 1;
-}
-*/
 cbuf* cbuf_create(int n){
     cbuf* cbuf = malloc(sizeof(cbuf));
     cbuf->bufferSize=n;
@@ -55,27 +41,28 @@ LINE cbuf_get(cbuf* cbuffer){
 int main (int argc, char** argv)
 {
     int lines = 10;
-    FILE *file;
-    if(strcmp(argv[1], "-n") == 0){
+    FILE *file = stdin;
+    if(argc > 2 && strcmp(argv[1], "-n") == 0){
         if(!isdigit(*argv[2])){
-            fprintf(stderr, "number of lines: '%s'\n", argv[2]);
+            fprintf(stderr, "Invalid value for argument -n\n");
             exit(1);
         }
         lines = atoi(argv[2]);
         if(lines == 0){
             exit(0);
         }
-        if(argc=3)
-            file = stdin;
+        if(argc == 4)
+            file = fopen(argv[3], "r");
+    }
+    else if (argc == 2)
+        file = fopen(argv[1], "r");
+        
+    if(file==NULL){
+        fprintf(stderr, "Failed to open file!\n");
+        exit(1);
     }
     printf("cbuffer size=%d\n", lines);
-    cbuf* cbuffer = cbuf_create(lines);
-    cbuf_put(cbuffer, "aaahhh");
-    cbuf_put(cbuffer, "aaa");
-    cbuf_put(cbuffer, "hhh");
-    cbuf_put(cbuffer, "bing bong");
-    for(int i=0; i<cbuffer->bufferSize; i++)
-        printf("%s\n", cbuf_get(cbuffer).c);
+    cbuf* cbuffer = cbuf_create(lines); 
     int value = 1001;
     /*while (put (value ++));
     while (get (& value))
