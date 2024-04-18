6ea07e86d074c07347a5eaba64ff6e77e9e3bc5a
Tadeas Horak <horaktada@seznam.cz>
2024-04-18 21:29:13 +0200

    check if malloc was successful

--- htab_lookup_add.c
+++ htab_lookup_add.c
@@ -1,6 +1,8 @@
 #include "htab.h"
 #include "htab_t.h"
 
+#include <stdio.h>
+
 htab_pair_t * htab_lookup_add(htab_t * t, htab_key_t key){
     htab_pair_t *pair = htab_find(t, key);
     if(pair!=NULL)
@@ -13,6 +15,10 @@ htab_pair_t * htab_lookup_add(htab_t * t, htab_key_t key){
             itm=itm->next;
         }
         htab_itm_t *newitm = malloc(sizeof(htab_itm_t));
+        if(newitm==NULL){
+            fprintf(stderr, "Memory allocation failed!\n");
+            exit(1);
+        }
         itm->next=newitm;
         newitm->next=NULL;
         newitm->pair.key=key;
