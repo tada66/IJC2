7917f7635a02ba833c134360a41186b43da94a45
Tadeas Horak <horaktada@seznam.cz>
2024-04-18 23:40:27 +0200

    clear done

--- htab_clear.c
+++ htab_clear.c
@@ -4,5 +4,14 @@
 #include <stdio.h>
 
 void htab_clear(htab_t * t){
-    
+    htab_itm_t *previtm = NULL;
+    for(int i=0; i<t->arr_size; i++){
+        htab_itm_t *itm = t->arr_ptr[i];
+        while(itm!=NULL){
+            previtm=itm;
+            itm=itm->next;
+            free(previtm->pair.key);
+            free(previtm);
+        }
+    }
 }
