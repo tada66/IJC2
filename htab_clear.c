4ecc054274dc3e5ed30a2e5da8151b4da013d09f
Tadeas Horak <horaktada@seznam.cz>
2024-04-18 22:47:24 +0200

    created

---
+++ htab_clear.c
@@ -0,0 +1,8 @@
+#include "htab.h"
+#include "htab_t.h"
+
+#include <stdio.h>
+
+void htab_clear(htab_t * t){
+    
+}
---
+++ htab_free.c
@@ -0,0 +1,8 @@
+#include "htab.h"
+#include "htab_t.h"
+
+#include <stdio.h>
+
+bool htab_free(htab_t * t){
+    
+}
