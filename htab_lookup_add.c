d57e29439664398232fbeec7256e9ca971654377
Tadeas Horak <horaktada@seznam.cz>
2024-04-18 22:28:14 +0200

    lookup working

--- htab_lookup_add.c
+++ htab_lookup_add.c
@@ -9,21 +9,34 @@ htab_pair_t * htab_lookup_add(htab_t * t, htab_key_t key){
         return pair;
     else{
         size_t hash = htab_hash_function(key);
-        int index = hash % t->arr_size;
+        size_t index = hash % t->arr_size;
         htab_itm_t *itm = t->arr_ptr[index];
-        while(itm->next!=NULL){
-            itm=itm->next;
+        if(itm!=NULL){
+            while(itm->next!=NULL){
+                itm=itm->next;
+            }
         }
         htab_itm_t *newitm = malloc(sizeof(htab_itm_t));
         if(newitm==NULL){
             fprintf(stderr, "Memory allocation failed!\n");
             exit(1);
         }
-        itm->next=newitm;
+        if(itm==NULL)
+            t->arr_ptr[index] = newitm;
+        else
+            itm->next = newitm;
+
         newitm->next=NULL;
-        newitm->pair.key=key;
-        newitm->pair.value=0;
+        char *keyString = malloc(sizeof(char) * (strlen(key)+1));
+        if(keyString==NULL){
+            fprintf(stderr, "Memory allocation failed!\n");
+            free(newitm);
+            exit(1);
+        }
+        strcpy(keyString, key);
+        newitm->pair.key=keyString;
+        newitm->pair.value=1;
         t->size++;
-        return newitm;
+        return &newitm->pair;
     }
 }
