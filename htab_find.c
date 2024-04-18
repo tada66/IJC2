90ba9d98d5ea489b1fb9ef504e5340063d81f74e
Tadeas Horak <horaktada@seznam.cz>
2024-04-18 21:15:46 +0200

    find hopefully working

---
+++ htab_find.c
@@ -0,0 +1,16 @@
+#include "htab.h"
+#include "htab_t.h"
+
+#include <stdio.h>
+
+htab_pair_t *htab_find(const htab_t * t, htab_key_t key){
+    size_t hash = htab_hash_function(key);
+    int index = hash % t->arr_size;
+    htab_itm_t *itm = t->arr_ptr[index];
+    while(itm!=NULL){
+        if(strcmp(key, itm->pair.key))
+            return &itm->pair;
+        itm=itm->next;
+    }
+    return NULL;
+}
