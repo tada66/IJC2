600065628ca61e42cfcca94ef11f658a3c128a3b
Tadeas Horak <horaktada@seznam.cz>
2024-04-20 01:40:47 +0200

    removed includes as we have libraries now

--- wordcount.c
+++ wordcount.c
@@ -3,19 +3,7 @@
 //#include <string.h>
 //#include <ctype.h>
 #include "htab.h"
-
 #include <time.h>
-#include "htab_t.h"
-#include "htab_init.c"
-#include "htab_find.c"
-#include "htab_size.c"
-#include "htab_erase.c"
-#include "htab_bucket_count.c"
-#include "htab_lookup_add.c"
-#include "htab_hash_function.c"
-#include "htab_clear.c"
-#include "htab_free.c"
-#include "htab_statistics.c"
 
 #define HASH_TABLE_SIZE 16
 
@@ -44,7 +32,7 @@ int main(){
         free(key);
     }
     htab_statistics(t);
-    if(htab_erase(t, "bing bong"));
+    if(htab_erase(t, "bing bong"))
         printf("successful erase of bing bong\n");
     printf("Size: %ld, Arr size: %ld\n", htab_size(t), htab_bucket_count(t));
     htab_free(t);
