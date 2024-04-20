a2a8b1f273461712ee605ce469cda18bb45a413f
Tadeas Horak <horaktada@seznam.cz>
2024-04-20 14:51:06 +0200

    update io.c to pass tests

--- io.c
+++ io.c
@@ -6,14 +6,17 @@ int read_word(char *s, int max, FILE *f){
     int c = 0;
     int i = 0;
     while((c = fgetc(f)) != EOF){
-        if(!isspace(c) && i<max-1)
+        if(!isspace(c) && i<max-1){
             s[i] = c;
-        else
-            break;
-        i++;
+            i++;
+        }
+        else{
+            if(i!=0)
+                break;
+        }
     }
     s[i+1] = '\0';
-    if(c==EOF)
+    if(c==EOF && i==0)
         return EOF;
     else
         return i;
