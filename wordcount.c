966bba3756e0b104cccf1414c54e0635d270fce2
Tadeas Horak <horaktada@seznam.cz>
2024-04-20 13:49:40 +0200

    should be fully working

--- wordcount.c
+++ wordcount.c
@@ -9,15 +9,26 @@
 #define HASH_TABLE_SIZE 16
 #define MAX_WORD_LENGTH 256 //255+1 for \0
 
+void print_count(htab_pair_t *data) {
+    printf("%s\t%d\n", data->key, data->value);
+}
 
 int main(){
     htab_t *t = htab_init(HASH_TABLE_SIZE);
-    char *c = malloc(sizeof(char) * MAX_WORD_LENGTH);
-    if(c = NULL){
+    char *word = malloc(sizeof(char) * MAX_WORD_LENGTH);
+    if(word == NULL){
         fprintf(stderr, "Failed to allocate memory!\n");
         exit(1);
     }
+    while (read_word(word, MAX_WORD_LENGTH, stdin)!=EOF)
+    {
+        htab_lookup_add(t, word);
+        memset(word, 0, sizeof(char) * MAX_WORD_LENGTH);
+    }
     
+    htab_for_each(t, &print_count);
+    htab_free(t);
+    free(word);
 
     return 0;
 }
